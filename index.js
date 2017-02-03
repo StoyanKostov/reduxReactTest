489639b9e3ce9182299beb4adb007070c2e1bf43
stoyan.kostov <stoyan.kostov@softwaregroup-bg.com>
2017-02-03 18:30:44 +0200

    second commit

--- index.js
+++ index.js
@@ -1,66 +1,71 @@
-var React = React;
-var render = ReactDOM;
-var store = Redux.createStore(counter);
-var Provider = ReactRedux.Provider;
+let React = React;
+let render = ReactDOM;
+let store = Redux.createStore(reducer);
+let Provider = ReactRedux.Provider;
+let connect  = ReactRedux.connect;
 
-function counter(state, action) {
+function reducer(state, action) {
+    console.log( action );
     if (typeof state === 'undefined') {
-        return 0
+        return { clicksCount: 0};
     }
     switch (action.type) {
         case 'INCREMENT':
-        return state + 1
+            return state + 1;
         case 'DECREMENT':
-        return state - 1
+            return state - 1;
         default:
-        return state
+            return state;
     }
 }
 
-  var CommentBox = React.createClass({
-        render: function(){
-            return (
-              React.DOM.div({className: "commentBox"}, 
-                "Hello, world! I am a CommentBox."
-              )
-            );
-        }
-    });
-    
-    ReactDOM.render(
-        <CommentBox/>,
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { clicksCount: 2 };
+    }
+
+    clickHandler(param) {
+        let { clicksCount } = this.state;
+        clicksCount += param;
+        this.setState({ clicksCount: clicksCount});
+        //this.props.dispatch( { type: 'INCREMENT', data: param } );
+    }
+
+    render() {
+        let { clicksCount } = this.state;
+        return (
+              <div>
+                  <h1>Hello, {this.props.appName}</h1>
+                  <p>Value {clicksCount}</p>
+                <p>
+                    <button onClick={() => { this.clickHandler(1); }}>+</button>
+                    <button onClick={() => { this.clickHandler(-1); }}>-</button>
+                </p>
+                </div>
+        );
+    }
+    };
+
+
+const mapStateToProps = (state, ownProps) => {
+    console.log(state, ownProps );
+  return {}
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+     console.log(ownProps );
+  return {}
+}
+
+const CommentBoxWrapper = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CommentBox);
+
+ReactDOM.render(
+        <Provider store={store}>
+            <CommentBoxWrapper appName="My first ReactRedux"/>
+        </Provider>,
         document.getElementById('content')
     );
-  
-
-var valueEl = document.getElementById('value');
-
- valueEl.innerHTML = store.getState().toString()
-
-// function render() {
-//     valueEl.innerHTML = store.getState().toString()
-// }
-
-// render()
-
-// store.subscribe(render)
-
-document.getElementById('increment').addEventListener('click', function () {
-    store.dispatch({ type: 'INCREMENT' });
-});
-
-document.getElementById('decrement').addEventListener('click', function () {
-    store.dispatch({ type: 'DECREMENT' });
-});
-
-document.getElementById('incrementIfOdd').addEventListener('click', function () {
-    if (store.getState() % 2 !== 0) {
-        store.dispatch({ type: 'INCREMENT' })
-    }
-});
-
-document.getElementById('incrementAsync').addEventListener('click', function () {
-    setTimeout(function () {
-        store.dispatch({ type: 'INCREMENT' })
-    }, 1000)
-})
--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var path = require('path');
 http.createServer(function (request, response) {
     var pathname = url.parse(request.url).pathname;
   response.writeHead(200);
-  fs.createReadStream( path.join( process.cwd(), pathname )).pipe(response)
+  fs.createReadStream( path.join( process.cwd(), pathname )).pipe(response);
 }).listen( 8080 );
 
